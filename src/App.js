3fafe6e4da47beccc98a25af06adec915500e81a
Meke Finau <lonifinau.09@gmail.com>
2022-04-13 19:52:35 -0600

    cake image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <h1>Amelia's Sweets Bakery</h1>
-        <img src='keke.png' className='App-logo' alt='logo' />
+        <h1>Amelia's Sweet Treats</h1>
+        <img
+          src={require('../src/images/keke.png')}
+          className='images'
+          alt='chocolate cakes'
+        ></img>
       </header>
     </div>
   );
